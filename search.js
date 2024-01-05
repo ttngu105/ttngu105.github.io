055e4e23cdeaf83a642343f857d74b6a844ccac5
ttngu105 <50807331+ttngu105@users.noreply.github.com>
2024-01-04 23:28:57 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,44 +1,67 @@
 // JavaScript code 
-/*hall = {}
-table = {}*/
-hall={"tommy":'1',"timmy":'2',"dad":'3',"mom":'3'};
-table={"tommy":'1',"timmy":'2',"dad":'3',"mom":'3'};
-/*table["tommy"] = "1"
-table["timmy"] = "2"
-table["dad"] = "3"
-table["mom"] = "3"*/
-function handle(e){
-    document.getElementById("name").onkeydown = function() {myFunction()};
-    /*if(e.keyCode !== null){
+
+fullname={'TAYLOR':['BILLY & KAI (4)', 'RYAN & ALLE'], 'SANDERS': ['DOUG & DEBBIE'], 'MEARIMAN': ['JEFFREY & KATHLEEN', 'JACOB'], 'VILLAVASO': ['BRAD & JERRI (5)', 'SAVANNAH & ANDREW LEBOEUF', 'CHAD & NANCY'], 'MANOR': ['MIMI'], 'ALDRIGE': ['JOSHUA'], 'JUDGE': ['COREY & LAUREN', 'TREVOR'], 'TRINH': ['ANDREW'], 'NGUYEN': ['CYNTHIA & TRI', 'TOMMY', 'LIEM', 'ROOK & LE (3)', 'JOHN & NGUYET', 'TAI & HUONG', 'CHUONG', 'TIEN & MAI', 'ALEX & TRINH', 'LINH & VAN', 'PHUONG', 'HOANG', 'TOAN & TRANG', 'HUNG & TAMMY', 'HUAN & THUY (5)', 'GIOAN & THUY (3)', 'O/B QUYEN', 'O/B TAN', 'HON', 'KHA', 'TAM', 'O/B TOAN', 'HOA', 'XUAN', 'DON', 'THI', 'TEO', 'MARS', 'O/B THONG', 'VINH', 'O/B MINH', 'KUONG & TUYET', 'NHA & THU', 'TAN THOA', 'TRUONG & THUY', 'THANH CAM', 'DOAN & HONG', 'KHIET THUY', 'HUONG (BA HON)', 'TU & KATHY', 'THANH & LIEN (3)', 'NGAN & LUYEN (4)', 'TOAN (HUONG)', 'BINH & HUE', 'NGA (2)', 'O/B SU', 'PHUC & VUNG', 'TRONG & THUY', 'DAT & DIEM', 'HON', 'O/B THU', 'DAI (3)', 'THI (THU)', 'LONG', 'LAM', 'TIN (ANH)', 'AN & TRANG', 'DAVID & HUONG', 'NAM & ANH', 'THANH & LOAN', 'LAC & LANG', 'KHIET & KHIEM', 'ASHLEY, LINDSEY, ISABEL', 'MAI LAN', 'ABIGAYLE', 'LEAH & ANGELICA', 'JULIELLE, MINDY, ANNIE', 'VIVIAN', 'JOSHUA', 'ANTHONY', 'BRANDON', 'TAM (brother)', 'ALEX'], 'AGUIRRE': ['CLARISSA & CRISTIAN RODRIQUEZ'], 'MELACON': ['DOUG & STACEY', 'CHADD & JULIE', 'REED'], 'HERBERT': ['TYLER & SARAH'], 'GAUTREAU': ['TROY & ANGELA', 'MICHAEL & LISA', 'KIRK & LORI', 'BRYCE & MAKENZIE', 'BEAU & SABREANNA', 'TIM & VICKIE', 'JOHNATHAN & ASHLEY', 'CHRIS & LINDSEY KELSHEIMER'], 'MURPHY': ['JOHN & TAMMY'], 'LE': ['PHUONG & VI (5)', 'HANH & HOA', 'SIMON PHAN'], 'CROCKETT': ['TODD& JENNIFER (3)'], 'DELAUNE': ['KIRK & LESLIE (3)'], 'BLUE': ['JOHNNY & RACHELLE'], 'DUGAN': ['MICHAEL & SHERRI'], 'DINH': ['DIANE', 'KHANH & JENNY (6)', 'BINH & TIN', 'QUOC & PHUONG'], 'PERRETT': ['CAROL & CHERYL'], 'DAVIS': ['OVID & AMIE'], 'HUERSTEL': ['BRYAN & CHRISTIE'], 'EVANS': ['BYRNE & LIZ'], 'AUCOIN': ['ANDRY & TABITHA'], 'STIERWALD': ['KEVIN & IRENE'], 'CORTEZ': ['RYAN & LENA'], 'HOSHSTELER': ['BILL'], 'EASTRIDGE': ['CLAY & BROOKE'], 'WILLIS': ['BRIAN & CHARLOTTE'], 'TRAN': ['JOSEPH MAN', 'THU & HANH', 'THAO & SMITH', 'TAI OANH', 'HONG (HON)', 'HOA & HUONG (4)', 'SAU & TIEN', 'NAM & HUYEN', 'KHANH', 'THIEN', 'LOI & TUYET', 'TIMMY & ANNA', 'TYLER & AUTUMN', 'SAU (2)', 'THANG (2)'], 'GREEN': ['BRIAN & KERA'], 'LEBLANC': ['DUDLEY & AMBER'], 'TRAN-NGUYEN': ['TOAN'], 'LUMETTA': ['DANIEL', 'TERRY & ANGELA', 'ERIC & DEVIN'], 'HATREL': ['BYRON & BONNIE', 'TYLER & MAGGIE BABCOCK'], 'KERNION': ['RICHIE & CAREY'], 'BRIGNAC': ['JEFF & KATE'], 'NEHBASS': ['GABRIEL & JESSICA'], 'INDEST': ['DAVID'], 'PFISTER': ['CRAIG & MELISSA'], 'GUILLOT': ['AL & KAREN'], 'RODRIGUE': ['BLAKE & KRISTIN'], 'BOUDREAUX': ['MICHAEL & BRANDY'], 'CANNON': ['SETH & GINI'], 'TRUONG': ['HUE'], 'POIRRIER': ['DWIGHT & DIANNA'], 'BELLO': ['TOMMY & ANGELIQUE (4)'], 'ROGERS': ['LEE & TARYN'], 'DUBUC': ['MICHAEL & AMY'], 'NIETO': ['JOEY & BROOKE'], 'HANLEY': ['MICHAEL & MARIA'], 'VEGA': ['KEN & VICKI'], 'PHAN': ['DUNG & LY'], 'PHU': ['DIEU & BINH'], 'PITRE': ['TIM & CHERIE'], 'GERBITZ': ['MICHAEL & JAMIE'], 'BROUSSARD': ['RICHARD & CHRISTI'], 'PHAM': ['YEN (HANG)', 'CHIEN & MAI'], 'DUONG': ['THANH & SU'], 'BUI': ['YEN (HUYEN)'], 'SANTA CRUZ': ['MARY GRACE & KALEB TALBOT'], 'RUPP': ['HALEY & KALEB MCNAIR'], 'GAROFALO': ['REMY & IVAN MARTINEZ'], 'MONTESDEOCA': ['ALEX'], 'CHAUVIN': ['EMILY & DANIEL BOSSE'], 'GRAY': ['MORGAN & SEAN MARINO'], 'ADAMS': ['MEREDITH & GRANT HAYDEN'], 'BOPP': ['BAILEY'], 'HOOVER': ['ELIZABETH & CAMERON NODIER'], 'TRIAY': ['AUSTIN'], 'BENOIT': ['MATTHEW & ELLEN'], 'HACKNEY': ['MATTHEW & MORGAN'], 'SIMON': ['MARCUS'], 'WARD': ['DUSTIN'], 'BUDENSKI': ['ROSS'], 'BREAUD': ['NATE & SARAH CANNON'], 'MITCHELL': ['KATIE'], 'LABAT': ['MEGAN'], 'MANNO': ['TIFFANY'], 'CROSBY': ['CAROLINE'], 'FABACHER': ['OLIVIA & PHIL WILLIAMS'], 'LUNDIN': ['BLAKE'], 'MORRISON': ['BLAKE'], 'BUSH': ['DYLAN & MALLORY CROUCH'], 'FRANCIONI': ['LARRY & HAYLEY'], 'CHAMPAGNE': ['GRANT & SARAH LAURELLI'], 'HOTARD': ['HOBIE & GRETA'], 'TASSIN': ['ZACK & COLLEEN'], 'SAUNDERS': ['PEYTON'], 'NEYREY': ['MARC'], 'CASTLE': ['CODY & BRITTANY ADCOX'], 'LECORNE': ['TRIP & MARY CATHERINE TOSO'], 'MINNARD': ['KALAN & MADDIE DEGRANGE'], 'ROBINSON': ['JACKIE & SHEFALI MITRA'], 'DESAI': ['RISHI & ANJALI PATEL'], 'HAMBERGER': ['DUANE & CINDY'], 'FORSYTHE': ['KEVIN & MARY'], 'SAAVEDRA': ['MARIO & HOPE'], '': ['DUNG & HOAN']};
+tables = {'TAYLOR, BILLY & KAI (4)': '1', 'TAYLOR, RYAN & ALLE': '1', 'SANDERS, DOUG & DEBBIE': '1', 'MEARIMAN, JEFFREY & KATHLEEN': '1', 'VILLAVASO, BRAD & JERRI (5)': '2', 'VILLAVASO, SAVANNAH & ANDREW LEBOEUF': '2', 'MANOR, MIMI': '2', 'VILLAVASO, CHAD & NANCY': '2', 'ALDRIGE, JOSHUA': '3', 'JUDGE, COREY & LAUREN': '3', 'JUDGE, TREVOR': '3', 'TRINH, ANDREW': '3', 'NGUYEN, CYNTHIA & TRI': '3', 'NGUYEN, TOMMY': '3', 'AGUIRRE, CLARISSA & CRISTIAN RODRIQUEZ': '3', 'MELACON, DOUG & STACEY': '4', 'HERBERT, TYLER & SARAH': '4', 'GAUTREAU, TROY & ANGELA': '4', 'MURPHY, JOHN & TAMMY': '4', 'MELACON, CHADD & JULIE': '4', 'LE, PHUONG & VI (5)': '5', 'CROCKETT, TODD& JENNIFER (3)': '5', 'NGUYEN, LIEM': '5', 'NGUYEN, ROOK & LE (3)': '5', 'GAUTREAU, MICHAEL & LISA': '6', 'NGUYEN, JOHN & NGUYET': '6', 'NGUYEN, TAI & HUONG': '6', 'DELAUNE, KIRK & LESLIE (3)': '6', 'NGUYEN, CHUONG': '9', 'NGUYEN, TIEN & MAI': '7', 'NGUYEN, ALEX & TRINH': '7', 'NGUYEN, LINH & VAN': '7', 'NGUYEN, PHUONG': '7', 'NGUYEN, HOANG': '7', 'NGUYEN, TOAN & TRANG': '7', 'NGUYEN, HUNG & TAMMY': '8', 'NGUYEN, HUAN & THUY (5)': '8', 'NGUYEN, GIOAN & THUY (3)': '8', 'NGUYEN, O/B QUYEN': '9', 'NGUYEN, O/B TAN': '9', 'NGUYEN, HON': '21', 'NGUYEN, KHA': '9', 'NGUYEN, TAM': '9', 'NGUYEN, O/B TOAN': '9', 'NGUYEN, HOA': '9', 'BLUE, JOHNNY & RACHELLE': '10', 'DUGAN, MICHAEL & SHERRI': '10', 'DINH, DIANE': '11', 'DINH, KHANH & JENNY (6)': '11', 'NGUYEN, XUAN': '11', 'NGUYEN, DON': '11', 'NGUYEN, THI': '11', 'NGUYEN, TEO': '11', 'NGUYEN, MARS': '11', 'PERRETT, CAROL & CHERYL': '12', 'DAVIS, OVID & AMIE': '12', 'HUERSTEL, BRYAN & CHRISTIE': '12', 'EVANS, BYRNE & LIZ': '12', 'AUCOIN, ANDRY & TABITHA': '12', 'NGUYEN, O/B THONG': '13', 'NGUYEN, VINH': '13', 'NGUYEN, O/B MINH': '13', 'NGUYEN, KUONG & TUYET': '13', 'NGUYEN, NHA & THU': '13', 'NGUYEN, TAN THOA': '13', 'STIERWALD, KEVIN & IRENE': '14', 'CORTEZ, RYAN & LENA': '14', 'HOSHSTELER, BILL': '14', 'EASTRIDGE, CLAY & BROOKE': '14', 'WILLIS, BRIAN & CHARLOTTE': '14', 'NGUYEN, TRUONG & THUY': '15', 'NGUYEN, THANH CAM': '15', 'NGUYEN, DOAN & HONG': '15', 'NGUYEN, KHIET THUY': '15', 'NGUYEN, HUONG (BA HON)': '15', 'NGUYEN, TU & KATHY': '15', 'TRAN, JOSEPH MAN': '15', 'GAUTREAU, KIRK & LORI': '16', 'GREEN, BRIAN & KERA': '16', 'LEBLANC, DUDLEY & AMBER': '16', 'GAUTREAU, BRYCE & MAKENZIE': '16', 'GAUTREAU, BEAU & SABREANNA': '16', 'GAUTREAU, TIM & VICKIE': '10', 'GAUTREAU, JOHNATHAN & ASHLEY': '10', 'GAUTREAU, CHRIS & LINDSEY KELSHEIMER': '10', 'TRAN, THU & HANH': '20', 'TRAN, THAO & SMITH': '20', 'NGUYEN, THANH & LIEN (3)': '18', 'NGUYEN, NGAN & LUYEN (4)': '18', 'DINH, BINH & TIN': '18', 'NGUYEN, TOAN (HUONG)': '18', 'NGUYEN, BINH & HUE': '19', 'TRAN-NGUYEN, TOAN': '19', 'TRAN, TAI OANH': '19', 'NGUYEN, NGA (2)': '19', 'LUMETTA, DANIEL': '17', 'HATREL, BYRON & BONNIE': '17', 'KERNION, RICHIE & CAREY': '17', 'BRIGNAC, JEFF & KATE': '17', 'NEHBASS, GABRIEL & JESSICA': '17', 'INDEST, DAVID': '14', 'NGUYEN, O/B SU': '21', 'NGUYEN, PHUC & VUNG': '21', 'NGUYEN, TRONG & THUY': '21', 'NGUYEN, DAT & DIEM': '21', 'TRAN, HONG (HON)': '21', 'PFISTER, CRAIG & MELISSA': '22', 'GUILLOT, AL & KAREN': '22', 'RODRIGUE, BLAKE & KRISTIN': '22', 'BOUDREAUX, MICHAEL & BRANDY': '22', 'CANNON, SETH & GINI': '22', 'NGUYEN, O/B THU': '23', 'NGUYEN, DAI (3)': '23', 'NGUYEN, THI (THU)': '23', 'NGUYEN, LONG': '23', 'NGUYEN, LAM': '23', 'TRUONG, HUE': '23', 'NGUYEN, TIN (ANH)': '23', 'POIRRIER, DWIGHT & DIANNA': '24', 'LUMETTA, TERRY & ANGELA': '24', 'LUMETTA, ERIC & DEVIN': '24', 'BELLO, TOMMY & ANGELIQUE (4)': '24', 'TRAN, HOA & HUONG (4)': '25', 'TRAN, SAU & TIEN': '25', 'NGUYEN, AN & TRANG': '25', 'NGUYEN, DAVID & HUONG': '25', 'ROGERS, LEE & TARYN': '26', 'DUBUC, MICHAEL & AMY': '26', 'NIETO, JOEY & BROOKE': '26', 'HANLEY, MICHAEL & MARIA': '26', 'VEGA, KEN & VICKI': '26', 'PHAN, DUNG & LY': '27', 'PHU, DIEU & BINH': '27', 'NGUYEN, NAM & ANH': '27', 'DINH, QUOC & PHUONG': '27', 'PITRE, TIM & CHERIE': '24', 'GERBITZ, MICHAEL & JAMIE': '28', 'BROUSSARD, RICHARD & CHRISTI': '28', 'NGUYEN, THANH & LOAN': '29', 'TRAN, NAM & HUYEN': '29', 'LE, HANH & HOA': '29', 'PHAM, YEN (HANG)': '29', 'TRAN, KHANH': '29', 'PHAM, CHIEN & MAI': '29', 'NGUYEN, LAC & LANG': '30', 'LE, SIMON PHAN': '30', 'DUONG, THANH & SU': '30', 'BUI, YEN (HUYEN)': '30', 'TRAN, THIEN': '30', 'NGUYEN, KHIET & KHIEM': '30', 'TRAN, LOI & TUYET': '30', 'NGUYEN, ASHLEY, LINDSEY, ISABEL': '31', 'NGUYEN, MAI LAN': '31', 'NGUYEN, ABIGAYLE': '31', 'NGUYEN, LEAH & ANGELICA': '31', 'NGUYEN, JULIELLE, MINDY, ANNIE': '31', 'NGUYEN, VIVIAN': '32', 'NGUYEN, JOSHUA': '32', 'NGUYEN, ANTHONY': '32', 'NGUYEN, BRANDON': '32', 'NGUYEN, TAM (brother)': '32', 'TRAN, TIMMY & ANNA': '32', 'TRAN, TYLER & AUTUMN': '32', 'NGUYEN, ALEX': '32', 'SANTA CRUZ, MARY GRACE & KALEB TALBOT': '33', 'RUPP, HALEY & KALEB MCNAIR': '33', 'GAROFALO, REMY & IVAN MARTINEZ': '33', 'MEARIMAN, JACOB': '33', 'HATREL, TYLER & MAGGIE BABCOCK': '33', 'MONTESDEOCA, ALEX': '33', 'CHAUVIN, EMILY & DANIEL BOSSE': '34', 'GRAY, MORGAN & SEAN MARINO': '34', 'ADAMS, MEREDITH & GRANT HAYDEN': '34', 'BOPP, BAILEY': '34', 'HOOVER, ELIZABETH & CAMERON NODIER': '34', 'MELACON, REED': '34', 'TRIAY, AUSTIN': '35', 'BENOIT, MATTHEW & ELLEN': '35', 'HACKNEY, MATTHEW & MORGAN': '35', 'SIMON, MARCUS': '35', 'WARD, DUSTIN': '35', 'BUDENSKI, ROSS': '35', 'BREAUD, NATE & SARAH CANNON': '35', 'MITCHELL, KATIE': '36', 'LABAT, MEGAN': '36', 'MANNO, TIFFANY': '36', 'CROSBY, CAROLINE': '36', 'FABACHER, OLIVIA & PHIL WILLIAMS': '36', 'LUNDIN, BLAKE': '36', 'MORRISON, BLAKE': '36', 'BUSH, DYLAN & MALLORY CROUCH': '36', 'FRANCIONI, LARRY & HAYLEY': '37', 'CHAMPAGNE, GRANT & SARAH LAURELLI': '37', 'HOTARD, HOBIE & GRETA': '37', 'TASSIN, ZACK & COLLEEN': '37', 'SAUNDERS, PEYTON': '37', 'NEYREY, MARC': '37', 'CASTLE, CODY & BRITTANY ADCOX': '38', 'LECORNE, TRIP & MARY CATHERINE TOSO': '38', 'MINNARD, KALAN & MADDIE DEGRANGE': '38', 'ROBINSON, JACKIE & SHEFALI MITRA': '38', 'DESAI, RISHI & ANJALI PATEL': '38', 'HAMBERGER, DUANE & CINDY': '28', 'FORSYTHE, KEVIN & MARY': '28', 'SAAVEDRA, MARIO & HOPE': '28', 'TRAN, SAU (2)': '20', 'TRAN, THANG (2)': '20', ' DUNG & HOAN': '27'}
+function lastname(e){
+    if(e.keyCode === 13){
         e.preventDefault(); // Ensure it is only this code that runs
-        let input = document.getElementById('name').onkeydown
-        input=input.toLowerCase()
+        let input = document.getElementById('lname').value
+        input=input.toUpperCase()
        // hall[input]
        const tablenum = document.getElementById('tablenumber');
        const hallnum = document.getElementById('hallnumber');
-       tablenum.innerHTML = table[input];
-       hallnum.innerHTML = hall[input];
+       const list = document.getElementById('firstnames');
+       list.style.display = "none";
+       //console.log(input);
+       //console.log(fullname[input]);
+       fullname[input].forEach((cname) => {
+        if(list){
+        list.append(Object.assign(document.createElement('a'),{id:cname},{href:"javascript:setTimeout(myFunction, 3)"},{textContent: cname},{onclick:search_table(lname,cname)}));
+        }
+       })
+       //tablenum.innerHTML = fullname[input];
+       //hallnum.innerHTML = fullname[input];
         //alert(hall[input] +" & "+table[input]);
-    }*/
+
+    }
 }
-function myFunction() {
- if (document.getElementById("name").value == t){
-  document.getElementById("name").style.backgroundColor = "red";}
-  else{
-  document.getElementById("name").style.backgroundColor = "blue";}
+function firstname(e){
+    if(e.keyCode === 13){
+    const lname = document.getElementById('lname').value.toUpperCase();
+    const tablenum = document.getElementById('tablenumber');
+    const hallnum = document.getElementById('hallnumber');
+    let fname = document.getElementById('fname');
+    fname = fname.value.toUpperCase();
+    console.log(fname)
+    tablenum.innerHTML = tables[lname+', '+fname];
+    hallnum.innerHTML = tables[lname+', '+fname];
+    }
+
+}
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("fname");
+    lname = document.getElementById('lname');
+    filter = input.value.toUpperCase();
+    console.log(filter);
+    div = document.getElementById("firstnames");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+       const list = document.getElementById('firstnames');
+       list.style.display = "";
+        txtValue = a[i].textContent || a[i].innerText;
+        console.log(txtValue.toUpperCase().indexOf(filter));
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            console.log(txtValue);
+          a[i].style.display = "";
+        } else {
+          a[i].style.display = "none";
+        }
+      }
   }
-}
-function search_person() { 
-    let input = document.getElementById('topnav').value 
-    input=input.toLowerCase(); 
-    let x = ["tommy","timmy","john","kim"]
-      
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="list-item";                  
-        } 
-    } 
+function search_table(lname,cname) { 
+    const tablenum = document.getElementById('tablenumber');
+    const hallnum = document.getElementById('hallnumber');
+    tablenum.value = tables[lname+', '+cname]
 } 
+
