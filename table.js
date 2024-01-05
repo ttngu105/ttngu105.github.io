8bd47f1b810257790d7c88bc394c02ab6dd6a3cc
ttngu105 <50807331+ttngu105@users.noreply.github.com>
2024-01-04 18:06:32 -0600

    Update table.js

--- table.js
+++ table.js
@@ -5,12 +5,6 @@ const sheet = encodeURIComponent('Sheet1');
 const urlGET = 'https://docs.google.com/spreadsheets/d/1CWyXmIUQKCyjabcGc1uOQGVD__riczejwyEeRir0gts/edit#gid=1737297764/'
 //const urlGET = `https://sheets.googleapis.com/v4/spreadsheets/1CWyXmIUQKCyjabcGc1uOQGVD__riczejwyEeRir0gts/values/${sheet}!a2:d2000?key=${key}`;
 const blogname = document.title;
-const postToGoogleDB = function(data){
-	const urlEncoded = encodeURL(urlPOST,data);
-	const request = initRequest("POST",urlEncoded,"no-cors");
-	sendRequest(request)
-		.then(responseEventPOST);
-}
 //get data
 const getFromGoogleDB = function(){
     const request = initRequest('GET', urlGET);
@@ -26,20 +20,44 @@ const encodeURL = function(path ,params){
 	}
 	return url;
 }
-const addComment= function(row){
 
-	const[time,name,message,sheettitle]=row
-	const chatList = document.getElementById('chat-list');
-	if (sheettitle == blogname){
-	chatList.innerHTML += `<li class="left-align no-bullets"><font color ="white"><div class ="card bg-dark card-text"><ul>Username: ${name}</ul>time: ${time}<ul >Message: \"${message}\"</font></ul></div></li>`
-	}
-}
 const initRequest = function (verb,url,mode='cors'){
 	const init = new Object();
 	init.method = verb;
 	init.mode = mode;
 	return new Request(url,init);
 }
+
+const sendRequest = async function(request){
+	const response = await fetch(request);
+	return response;
+}
+
+const responseEventGET = async function(response){
+    var sheetData = await response.json();
+    showComments(sheetData);
+}
+
+const showComments = function(data){
+	clearChat();
+	for(row of data.values){
+		addComment(row)
+	}
+}
+
+const addtable= function(row){
+	const[time,name,message,sheettitle]=row
+	const chatList = document.getElementById('chat-list');
+	if (sheettitle == blogname){
+	chatList.innerHTML += `<li class="left-align no-bullets"><font color ="white"><div class ="card bg-dark card-text"><ul>Username: ${name}</ul>time: ${time}<ul >Message: \"${message}\"</font></ul></div></li>`
+	}
+}
+const clearChat = function(){
+	const chatlist = document.getElementById('chat-list');
+	chatlist.innerHTML="";
+}
+
+
 const initViews = function(){
 	getFromGoogleDB();
 }
