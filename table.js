dbd7bbace59b654bcb43ba904efb8605f68c5700
ttngu105 <50807331+ttngu105@users.noreply.github.com>
2024-01-04 18:00:59 -0600

    Update table.js

--- table.js
+++ table.js
@@ -1,14 +1,9 @@
-// data for post request
-const formId = '1FAIpQLScyoiZntCX8uDACL1D_VQkXBuC8Dv6iO_LovkX4x1DddIEemg';
-const name = 'entry.130530209';
-const message = 'entry.1159817613';
-const title = 'entry.2000012467';
-const urlPOST = `https://docs.google.com/forms/d/e/${formId}/formResponse`;
 //data for get request
 let sheetId = '1CWyXmIUQKCyjabcGc1uOQGVD__riczejwyEeRir0gts';
 const key = 'AIzaSyAsXJcdU0jV8bynU6IMWPIo7JdoB8CuD8Y';
 const sheet = encodeURIComponent('Sheet1');
-const urlGET = `https://sheets.googleapis.com/v4/spreadsheets/1CWyXmIUQKCyjabcGc1uOQGVD__riczejwyEeRir0gts/values/${sheet}!a2:d2000?key=${key}`;
+const urlGET = 'https://docs.google.com/spreadsheets/d/1CWyXmIUQKCyjabcGc1uOQGVD__riczejwyEeRir0gts/edit#gid=1737297764/'
+//const urlGET = `https://sheets.googleapis.com/v4/spreadsheets/1CWyXmIUQKCyjabcGc1uOQGVD__riczejwyEeRir0gts/values/${sheet}!a2:d2000?key=${key}`;
 const blogname = document.title;
 const postToGoogleDB = function(data){
 	const urlEncoded = encodeURL(urlPOST,data);
@@ -16,6 +11,7 @@ const postToGoogleDB = function(data){
 	sendRequest(request)
 		.then(responseEventPOST);
 }
+//get data
 const getFromGoogleDB = function(){
     const request = initRequest('GET', urlGET);
     sendRequest(request)
@@ -30,10 +26,23 @@ const encodeURL = function(path ,params){
 	}
 	return url;
 }
+const addComment= function(row){
 
+	const[time,name,message,sheettitle]=row
+	const chatList = document.getElementById('chat-list');
+	if (sheettitle == blogname){
+	chatList.innerHTML += `<li class="left-align no-bullets"><font color ="white"><div class ="card bg-dark card-text"><ul>Username: ${name}</ul>time: ${time}<ul >Message: \"${message}\"</font></ul></div></li>`
+	}
+}
 const initRequest = function (verb,url,mode='cors'){
 	const init = new Object();
 	init.method = verb;
 	init.mode = mode;
 	return new Request(url,init);
 }
+const initViews = function(){
+	getFromGoogleDB();
+}
+const responseEventPOST = response => getFromGoogleDB();
+
+initViews();
